ec4adb06c0259799696675497f93b16020e164f1
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-12-08 23:50:28 +0300

    Update Dashboard.Designer.cs

--- Event Management System-EDP/Dashboard.Designer.cs
+++ Event Management System-EDP/Dashboard.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            
             this.panel1 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
